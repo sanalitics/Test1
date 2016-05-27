1c509ef2fe7efba48cf5747358e5dc374f78eca0
sanalitics <sanalitics@gmail.com>
2016-05-27 13:45:08 +1200

    Initial Checkin to Local Rep

---
+++ Test/src/test.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * @author sandeep.balugodu
+ *
+ */
+public class test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Testing");
+
+	}
+
+}
